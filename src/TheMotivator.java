f590c516f797c7de055f09408dabeca5a5117311
Madeline Geesen <madelinegeesen@mymail.mines.edu>
2018-10-16 14:01:09 -0600

    changed messages for each score

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -1,13 +1,13 @@
 public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
-			System.out.println("You're awesome");
+			System.out.println("Perfect score!");
 		else if (score > 90)
-			System.out.println("That's great");
+			System.out.println("Impressive.");
 		else if (score > 60)
-			System.out.println("That's good ");
+			System.out.println("At least you passed.");
 		else
-			System.out.println("Well, what can I say?");
+			System.out.println("Oops.");
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
