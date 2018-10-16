f89a1f71c1e511a807b7465fb4db9838a116b436
Madeline Geesen <madelinegeesen@mymail.mines.edu>
2018-10-16 14:05:48 -0600

    changed scores in if statements

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -2,10 +2,12 @@ public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
 			System.out.println("Perfect score!");
-		else if (score > 90)
+		else if (score > 80)
 			System.out.println("Impressive.");
-		else if (score > 60)
+		else if (score > 70)
 			System.out.println("At least you passed.");
+		else if (score > 60)
+			System.out.println("You can do better.");
 		else
 			System.out.println("Oops.");
 	}
